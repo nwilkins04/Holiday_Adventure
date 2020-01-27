3d07bab06fcfdaef19e34becd4d94f8e3f3ca5a1
Nicole W <Nicole.Wilkins2018@gmail.com>
2020-01-27 16:55:24 -0500

    more attempt for click button

--- assets/javascript/winter.js
+++ assets/javascript/winter.js
@@ -1,8 +1,6 @@
 console.log("winter page loaded")
 
-var snowman = $("snowmanImg");
 
 $("#snowmanImg").on("click", function() {
     console.log("snowman clicked");
-//    Math.floor(Math.random() * 1000) + 20; 
 });
