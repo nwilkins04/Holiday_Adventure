d15604a0902ec2fbccad49d997942cfcbdcc4e56
Nicole W <Nicole.Wilkins2018@gmail.com>
2020-01-30 12:08:40 -0500

    ghost names added

--- assets/javascript/spooky.js
+++ assets/javascript/spooky.js
@@ -2,7 +2,8 @@ console.log("spooky list connected")
 
 $("#nameGenerator").on("click", function() {
     var ghostName = "";
-    var ghostList = ["King Hamlet", "The Flying Dutchman", "The Bell Witch", "Casper", ""]
+    var ghostList = ["King Hamlet", "The Flying Dutchman", "The Bell Witch", "Casper", "Bloody Mary", "The Drury Lane Ghost", "The Vanishing Hitchhiker", "Ghost of Christmas Past", "The Murdered Peddler", "Slimer"];
+    
     for (var i = 0; i++;) {
 
     }
