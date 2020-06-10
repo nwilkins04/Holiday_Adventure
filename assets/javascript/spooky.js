5b77a40325f04bc8abf9628b672a90adb0e56bb8
Nicole W <Nicole.Wilkins2018@gmail.com>
2020-06-09 22:45:56 -0400

    video works

--- assets/javascript/spooky.js
+++ assets/javascript/spooky.js
@@ -3,8 +3,8 @@ console.log("spooky list connected")
 $("#nameGenerator").on("click", function() {
     var ghostName = "";
     var ghostList = ["King Hamlet", "The Flying Dutchman", "The Bell Witch", "Casper", "Bloody Mary", "The Drury Lane Ghost", "The Vanishing Hitchhiker", "Ghost of Christmas Past", "The Murdered Peddler", "Slimer"];
-    
+
     for (var i = 0; i++;) {
 
     }
-})
+});
